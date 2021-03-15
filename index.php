01f384994304e14f9edf0a4e07f7f20429d21cc2
Mitro09 <mpatrick94@libero.it>
2021-03-15 17:38:15 +0100

    costruito il form

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+
+require "./lib/JSONReader.php";
+
+$taskList = JSONReader('./dataset/TaskList.json');
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Tasklist</title>
+</head>
+<body>
+
+    <div class="container-fluid bg-secondary py-3 mb-3 text-light">
+        <div class="container">
+            <h1 class="display-1">Tasklist</h1>
+        </div>
+    </div>
+ 
+    <div class="container">
+        <div class="input-group pb-3 my-1">
+            <label class="w-100 pb-1 fw-bold" for="searchText">Cerca</label>
+            <input name ="searchText" id="searchText"  type="text" class="form-control" placeholder="attività da cercare" value="<?=$searchText ?>">
+            <div class="input-group-append">
+              <button class="btn btn-primary" type="button">Invia</button>
+            </div>
+        </div>
+
+        <div id="status-radio" class=" mb-3">
+            <div class="fw-bold pe-2 w-100">Stato attività</div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="status" value="all">
+                <label class="form-check-label" >tutti</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="status" value="todo">
+                <label class="form-check-label" >da fare</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="status" value="progress">
+                <label class="form-check-label" >in lavorazione</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="status" value="done">
+                <label class="form-check-label" >fatto</label>
+              </div>
+        </div>
+    
+        <section class="tasklist mt-3">
+            <h1 class="fw-bold fs-6">Elenco delle attività</h1>
+            <table class="table">
+                <tr>
+                    <th class="w-100">nome</th>
+                    <th class="text-center">stato</th>
+                    <th class="text-center">data</th>
+                </tr>
+                <?php
+                foreach ($taskList as $key => $task){
+                    $status = $task['status'];
+                    $taskName = $task['taskName'];
+                ?>
+                
+                <tr>
+                    <td><?=$taskName?></td>
+                    <td class="text-center">
+                        <span class="<?php if ($status=="todo"){
+                            echo "badge bg-danger text-uppercase";
+                            }
+                            elseif( $status=="done"){
+                                echo "badge bg-secondary text-uppercase";
+                            }
+                            else{
+                                echo "badge bg-primary text-uppercase";
+                            }
+                            ?>"><?=$status?></span>
+                    </td>
+                    <td class="text-nowrap">
+                        3 Luglio
+                    </td>
+                </tr>
+                <!--<tr>
+                    <td>Comprare la farina</td>
+                    <td class="text-center">
+                        <span class="badge bg-secondary text-uppercase">done</span>
+                    </td>
+                    <td class="text-nowrap">
+                        20 Settembre
+                    </td>
+                </tr>
+                <tr>
+                    <td>Comprare la farina</td>
+                    <td class="text-center">
+                        <span class="badge bg-primary text-uppercase">progress</span>
+                    </td>
+                    <td class="text-nowrap">
+                        18 Settembre
+                    </td>
+                </tr>-->
+                <?php } ?>
+            </table>
+
+        </section>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
